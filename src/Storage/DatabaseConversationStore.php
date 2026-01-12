3441b36328fee3c9dae9ab7fdf8350f317a491d6
Taylor Otwell <taylor@laravel.com>
2026-01-12 16:52:33 -0600

    wip

--- src/Storage/DatabaseConversationStore.php
+++ src/Storage/DatabaseConversationStore.php
@@ -20,7 +20,7 @@ class DatabaseConversationStore implements ConversationStore
         DB::table('agent_conversations')->insert([
             'id' => $conversationId,
             'user_id' => $userId,
-            'title' => Str::limit($title, 100),
+            'title' => Str::limit($title, 100, preserveWords: true),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
