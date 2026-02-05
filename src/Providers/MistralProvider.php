59703fba4dec8564108fa7866ae08ab01b59ed0e
Jack <jjbayliss@icloud.com>
2026-02-05 23:34:17 +0000

    EmbeddingProvider

--- src/Providers/MistralProvider.php
+++ src/Providers/MistralProvider.php
@@ -2,13 +2,16 @@
 
 namespace Laravel\Ai\Providers;
 
+use Laravel\Ai\Contracts\Providers\EmbeddingProvider;
 use Laravel\Ai\Contracts\Providers\TextProvider;
 use Laravel\Ai\Contracts\Providers\TranscriptionProvider;
 
-class MistralProvider extends Provider implements TextProvider, TranscriptionProvider
+class MistralProvider extends Provider implements EmbeddingProvider, TextProvider, TranscriptionProvider
 {
+    use Concerns\GeneratesEmbeddings;
     use Concerns\GeneratesText;
     use Concerns\GeneratesTranscriptions;
+    use Concerns\HasEmbeddingGateway;
     use Concerns\HasTextGateway;
     use Concerns\HasTranscriptionGateway;
     use Concerns\StreamsText;
@@ -43,5 +46,21 @@ class MistralProvider extends Provider implements TextProvider, TranscriptionPro
     public function defaultTranscriptionModel(): string
     {
         return 'voxtral-small-latest';
+    }
+
+    /**
+     * Get the name of the default embeddings model.
+     */
+    public function defaultEmbeddingsModel(): string
+    {
+        return 'mistral-embed';
+    }
+
+    /**
+     * Get the default dimensions of the default embeddings model.
+     */
+    public function defaultEmbeddingsDimensions(): int
+    {
+        return 1024;
     }
 }
