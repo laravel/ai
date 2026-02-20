14f263e3fa712d5f2bab42b024e5dd6fc3445e36
Sabri Taieb <sabritaiebdev@gmail.com>
2026-02-20 07:45:34 +0100

    chore: fix Pint issues

--- src/Responses/StreamableAgentResponse.php
+++ src/Responses/StreamableAgentResponse.php
@@ -4,13 +4,13 @@ namespace Laravel\Ai\Responses;
 
 use Closure;
 use Illuminate\Contracts\Support\Responsable;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Collection;
 use IteratorAggregate;
 use Laravel\Ai\Responses\Data\Meta;
 use Laravel\Ai\Responses\Data\Usage;
 use Laravel\Ai\Streaming\Events\StreamEnd;
 use Laravel\Ai\Streaming\Events\TextDelta;
+use Symfony\Component\HttpFoundation\Response;
 use Traversable;
 
 class StreamableAgentResponse implements IteratorAggregate, Responsable
@@ -96,7 +96,6 @@ class StreamableAgentResponse implements IteratorAggregate, Responsable
      * Create an HTTP response that represents the object.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function toResponse($request): Response
     {
