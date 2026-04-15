c89dc1119f23acfc688486381378c1b4fd137692
Alejandro Baldominos <alejandro.baldominos@smileandlearn.com>
2026-04-15 14:02:22 +0200

    New image formats in Gemini

--- src/Gateway/Gemini/GeminiGateway.php
+++ src/Gateway/Gemini/GeminiGateway.php
@@ -130,7 +130,7 @@ class GeminiGateway implements Gateway
      * Generate an image.
      *
      * @param  array<ImageFile>  $attachments
-     * @param  '21:9'|'16:9'|'9:16'|'5:4'|'4:5'|'4:3'|'3:4'|'3:2'|'2:3'|'1:1'  $size
+     * @param  '21:9'|'16:9'|'9:16'|'8:1'|'5:4'|'4:5'|'4:3'|'4:1'|'3:4'|'3:2'|'2:3'|'1:8'|'1:4'|'1:1'  $size
      * @param  'xlow'|'low'|'medium'|'high'  $quality
      */
     public function generateImage(
