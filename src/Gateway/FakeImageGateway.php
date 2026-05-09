b1cdf2f140b4559918474f48ae588d2ac9f5c462
Pushpak Chhajed <pushpak1300@gmail.com>
2026-05-09 11:25:33 +0530

    Formatting

--- src/Gateway/FakeImageGateway.php
+++ src/Gateway/FakeImageGateway.php
@@ -28,7 +28,7 @@ class FakeImageGateway implements ImageGateway
      * Generate an image.
      *
      * @param  array<Image>  $attachments
-     * @param  'low'|'medium'|'high'  $quality
+     * @param  'low'|'medium'|'high'|null  $quality
      */
     public function generateImage(
         ImageProvider $provider,
